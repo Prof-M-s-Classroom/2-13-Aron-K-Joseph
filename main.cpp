8d8d7286123239d43a9ed5e2532ddf1d308614f7
Aron-Kaniyarakath-Joseph <ajoseph2148@sdsu.edu>
2025-02-16 09:29:24 -0800

    Updated Del last and reverse list

--- main.cpp
+++ main.cpp
@@ -3,161 +3,166 @@ using namespace std;
 
 class student {
 public:
-    string name;
-    int age;
+  string name;
+  int age;
 
-    student(string name, int age) {
-        this->name = name;
-        this->age = age;
-    }
-    void print() { cout << name << " " << age << endl; }
+  student(string name, int age) {
+    this->name = name;
+    this->age = age;
+  }
+  void print() { cout << name << " " << age << endl;}
 };
-
 template <typename T> class Node {
-public:
+  public:
     T *value;
     Node<T> *next;
     Node(T *value) {
-        this->value = value;
-        this->next = NULL;
+      this->value = value;
+      this->next = NULL;
     }
     void print() { value->print(); }
 };
 
 template <typename T> class LinkedList {
-private:
+  private:
     int length;
-    Node<T> *head;
+    Node<T>* head;
 
-public:
+  public:
     LinkedList(T *value) {
-        this->length = 1;
-        this->head = new Node<T>(value);
+      this->length = 1;
+      this->head = new Node<T>(value);
     }
     void add(T *value) {
-        Node<T> *newNode = new Node<T>(value);
-        Node<T> *temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-        length++;
+      Node<T> *newNode = new Node<T>(value);
+      Node<T> *temp = head;
+      while (temp->next != NULL) {
+        temp = temp->next;
+      }
+      temp->next = newNode;
+      length++;
     }
 
     void addhead(T *value) {
-        Node<T> *newNode = new Node<T>(value);
-        newNode->next = head;
-        head = newNode;
-        length++;
+      Node<T> *newNode = new Node<T>(value);
+      newNode->next = head;
+      head = newNode;
+      length++;
     }
 
     void delfirst() {
-        Node<T> *temp = head;
-        head = head->next;
-        delete temp;
-        length--;
+      Node<T> *temp = head;
+      head = head->next;
+      delete temp;
+      length--;
     }
 
     void dellast() {
-        Node<T> *temp = head;
-        while (temp->next!=NULL)
-            temp = temp->next;
-        delete temp;
-        length--;
+      Node<T> *temp = head;
+      while (temp->next->next != NULL)
+        temp = temp->next;
+      delete temp->next;
+      temp->next = NULL;
+      length--;
     }
 
-    Node<T>* get(int index) {
-        if (index < 0 || index>=length) {
-            return nullptr;
-        }
-        Node<T>* temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp->next;
-        }
-        return temp;
+    Node<T> *get(int index) {
+      if (index < 0 || index >= length) {
+        return nullptr;
+      }
+      Node<T> *temp = head;
+      for (int i = 0; i < index; i++) {
+        temp = temp->next;
+      }
+      return temp;
     }
 
     void deleteNode(int index) {
-       //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
-        if ((index > length-1) || (index < 0)) {
-            throw std::out_of_range("index out of range");
-        }
-        else if (index == 0) {
-            delfirst();
-        }
-        else if (index == length-1) {
-            dellast();
-        }
-        else {
-            Node<T>* currnode = this->head;
-            for (int i = 0; i < index-1; i++) {
-                currnode = currnode->next;
-            }
-            Node<T>* temp = get(index - 1);
-            Node<T>* del = temp->next;
-            temp->next = temp->next->next;
-            delete del;
+      // TODO:Write the function to delete at the given index. Reuse the
+      // pre-written functions for edge cases. Account for missing index.
+      if ((index > length - 1) || (index < 0)) {
+        throw std::out_of_range("index out of range");
+      } else if (index == 0) {
+        delfirst();
+      } else if (index == length - 1) {
+        dellast();
+      } else {
 
-        }
+        Node<T> *prev = get(index - 1);
+        Node<T> *temp = prev->next;
+        prev->next = prev->next->next;
+        delete temp;
+      }
+      length--;
     }
 
-   void insert(int index, T *value) {
-        //TODO:Write a function to insert a new node at a give index. Reuse the pre-written functions for edge cases. Account for missing index
-        if (index < 0 || index > length) {
-            return;
-        }
-        if (index == 0) {
-            addhead(value);
-        }
-        else if (index == length) {
-            add(value);
-        }
-        else {
-            Node<T>* newNode = new Node<T>(value);
-            Node<T>* temp = get(index - 1);
-            newNode->next = temp->next;
-            temp->next = newNode;
-            length++;
-
-        }
+    void insert(int index, T *value) {
+      // TODO:Write a function to insert a new node at a give index. Reuse the
+      // pre-written functions for edge cases. Account for missing index
+      if (index < 0 || index > length) {
+        return;
+      }
+      if (index == 0) {
+        addhead(value);
+      } else if (index == length) {
+        add(value);
+      } else {
+        Node<T> *newNode = new Node<T>(value);
+        Node<T> *temp = get(index - 1);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        length++;
+      }
     }
 
-
-   void reverselist(){
-        //TODO:Write a function to reverse the list using the logic from the slide.
-        Node<T>* prev, curr, next;
-        prev = NULL;
-        curr = head;
-        while(curr!=NULL)
-        {
-            next = curr->nextNode;
-            curr->nextNode = prev;
-            prev = curr;
-            curr = next;
-        }
-        head = prev;
+    void reverselist() {
+      // TODO:Write a function to reverse the list using the logic from the slide.
+      Node<T> *prev, *curr, *next;
+      prev = NULL;
+      curr = head;
+      while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+      }
+      head = prev;
     }
 
     void print() {
-        cout << "Printing List.." << endl;
-        Node<T> *temp = head;
-        while (temp != NULL) {
-            temp->print();
-            temp = temp->next;
-        }
+      cout << "Printing List.." << endl;
+      Node<T> *temp = head;
+      while (temp != NULL) {
+        temp->print();
+        temp = temp->next;
+      }
     }
 };
 
 int main() {
-    student *s1 = new student("A", 20);
-    student *s2 = new student("B", 21);
-    student *s3 = new student("C", 22);
-    LinkedList<student> *ll = new LinkedList<student>(s1);
-    ll->add(s2);
-    ll->addhead(s3);
-    ll->print();
-    ll->delfirst();
-    ll->print();
-    ll->dellast();
-    ll->print();
+  student *s1 = new student("A", 20);
+  student *s2 = new student("B", 21);
+  student *s3 = new student("C", 22);
+
+  LinkedList<student> *ll = new LinkedList<student>(s1);
+  int var1 = 1;
+  var1 = 2;
+  ll -> add(s2);
+  ll -> add(s3);
+  ll->print();
+  ll->reverselist();
+  ll->print();
+  ll->addhead(s3);
+  ll ->dellast();
+  ll->print();
+  ll -> insert(1,s1);
+  ll->addhead(s3);
+  ll->addhead(s1);
+  ll->addhead(s2);
+  ll->print();
+  ll->deleteNode(2);
+  ll->deleteNode(4);
+  ll->print();
+  ll->reverselist();
+  ll->print();
 }
